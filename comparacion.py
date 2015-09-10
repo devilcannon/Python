28ffb47b7cd7f35198db6333812b24dd6e7a1c3b
Jesús Navarro <algoritmonavarro@gmail.com>
2015-09-10 12:20:10 -0500

    Algoritmo de comparación
    Pide dos números al usuario e imprime el mayor.

---
+++ comparacion.py
@@ -0,0 +1,8 @@
+x = int(input("Introduce un numero: "))
+y= int(input("Introduce otro numero: "))
+if(x>y):
+	print(x," es mayor que",y)
+else:
+		print(y, "Es mayor que ",x)
+print("Gracias por participar :D")
+		
